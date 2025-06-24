30cf44af6e1950e1b9f0946e77934712251aa156
Elfredy <mac@Ferids-MacBook-Pro.local>
2025-06-25 02:50:01 +0400

    sdf

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold">Hakkımda</h1>
+      <p>Buraya kendinle ilgili bilgileri yazabilirsin Kralım 👑</p>
+    </main>
+  );
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold">Contact</h1>
+      <p>Buraya kendinle ilgili bilgileri yazabilirsin Kralım 👑</p>
+    </main>
+  );
+}
